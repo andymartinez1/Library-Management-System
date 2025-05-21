6cde7d6de8514c59b77419a7343b27defefc77ca
Andrew Martinez <andy72391@gmail.com>
2025-05-21 06:24:34 -0400

    Refactored to include OOP concepts

---
+++ UserInterface.cs
@@ -0,0 +1,34 @@
+﻿using Spectre.Console;
+
+namespace LibraryManagementSystem;
+
+internal class UserInterface
+{
+    private BooksController _booksController = new BooksController();
+
+    internal void MainMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<Enums.MenuOption>()
+                    .Title("What do you want to do?")
+                    .AddChoices(Enum.GetValues<Enums.MenuOption>()));
+
+            switch (choice)
+            {
+                case Enums.MenuOption.ViewBooks:
+                    _booksController.ViewBooks();
+                    break;
+                case Enums.MenuOption.AddBook:
+                    _booksController.AddBook();
+                    break;
+                case Enums.MenuOption.DeleteBook:
+                    _booksController.DeleteBook();
+                    break;
+            }
+        }
+    }
+}
